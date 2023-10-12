317a6dbaa3ebe010e0b6d1efc778a88cb8d003e8
jfNatanahel <natanahel.jf@gmail.com>
2023-10-12 17:21:37 -0300

    revisado

--- 3)ejercicio.py
+++ 3)ejercicio.py
@@ -1,22 +1,18 @@
 n=int(input("Ingresar la cantidad de vector/lista: "))
 v=[0]*n
-v_aux=[0]*n
 i=0
 while i<n:
     v[i]=int(input("Ingresar los elementos del vector: "))
-    v[i]=v_aux
     i=i+1
-v_nuevo=[0]*n
+#Vector para almacenar los numeros capicuas
+v_nuevo=[] #vacio
 i=0
-j=0
-y=0
-while i<=n:
-    r=v[i]%10
-    y=y*10+r
-    v[i]=v[i]//10
-if v[i]==v_aux[i]:
-    v_aux=v_nuevo
-    j=j+1
+while i<n:
+    num=v[i] #el numero en la posicion i la guardo en una variable num.
+    num_str=str(num)#convierte el numero en una cadena de caracteres. 
+    if num_str==num_str[::-1]: #Comprobar si el numero es capicua
+                            #[::-1] -> para obtener una version invertida de la cadena. Ej:112 -> 211
+        v_nuevo.append(num)
     i=i+1
-print("El vector con los numeros capicuas son:",v_nuevo)
+print("El vector con los numeros capicuas es: ",v_nuevo)
 
