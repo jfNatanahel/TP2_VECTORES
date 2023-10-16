d13177890a36b566e6d3af70083d7c2099942795
jfNatanahel <natanahel.jf@gmail.com>
2023-10-16 15:36:23 -0300

    revisado

--- conceptos/8)Ordenamientos.py
+++ conceptos/8)Ordenamientos.py
@@ -16,15 +16,33 @@ print(x)"""
 #2.BURBUJA (va de 2 en 2):
 #Funciona reevisando cada elemento de la lista que va a ser ordenada con el siguiente, intercambiandolos
 #de posicion si estan en el orden equivocado. Finaliza cuando encuentre  el menor de todos.
-n=int(input("Ingresar la cantidad de numeros: "))
+"""n=int(input("Ingresar la cantidad de numeros: "))
 x=[None]*n
 for i in range (n):
     x[i]=int(input("Ingresar los elementos: "))
 print(x)
 for i in range(0,n):
     for j in range(0,n-i-1):
-        if x[j]>x[j+1]:
-            aux=x[j]
+        if x[j]>x[j+1]: #Posicion j=0, j[j+1] posicion j=1
+            aux=x[j] #Guardar el valor de x[j] posicion 0 para no perderlo.
             x[j]=x[j+1]
             x[j+1]=aux
+print(x)"""
+
+#3.INSERCION.
+#Este metdo es uno de los mas optimos, ya que a medida que van ingresando los numeros, la lista se va
+#ordenando. Como su nombre lo indica, cada vez que se ingresa un numero nuevo, el algoritmo detecta en que
+#posicion de la sublista ordenada debe ubicarse. Luego de ello, realiza un corrimiento dejando la ubi
+#exacta para insertar el numero en el lugra correspondiente.
+n=int(input("Ingresar la cantidad de numeros: "))
+x=[None]*n
+j=0
+for i in range (n):
+    x[i]=int(input("Ingresar los elementos: "))
+    v=x[i]
+    j=j-1
+    while j>=0 and x[j]>v:
+        x[j+1]=x[j]
+        j=j-1
+    x[j+1]=v
 print(x)
