7bfaef854f2d2542d0533a7f0b1dfc431085e5e2
jfNatanahel <natanahel.jf@gmail.com>
2023-10-16 22:07:35 -0300

    revisado

--- 6)Ejercicio.py
+++ 6)Ejercicio.py
@@ -6,8 +6,9 @@ n=int(input("Ingresar el tamaño de la LISTA2: "))
 lista2=[None]*n
 for j in range(n):
     lista2[j]=input("Ingersar caracteres L2: ")
-for i in range(m):
-    for j in range(n):
+for i in range(m): #recorrer la primer lista
+    for j in range(n): #ciclo anidado, recorrer la segunda lista
         if lista1[i]==lista2[j]:
             print(f"Carater {lista2[j]} LISTA 2 se encuentra en la LISTA 1")
-    
+#El programa puede tener un pequeño error, si existen dos elementos iguales se va a imprimir dos veces
+#aun asi sigue siendo optimo....
---
+++ conceptos/CONJUNTOS.py
@@ -0,0 +1,30 @@
+#1.UNION
+n=int(input("Ingresar tamaño lista 1: "))
+a=[None]*n
+for i in range(n):
+    a[i]=int(input("Ingresar los elementos L1"))
+m=int(input("Ingresar tamaño lista 1: "))
+b=[None]*m
+for j in range(m):
+    b[j]=int(input("Ingresar los elementos L1"))
+c=[]
+for i in range(n):#primer vector
+    existe=0
+    for j in range(m):#segundo vector
+        if a[i]==b[j]:
+            existe=1
+    if existe==0:
+        c.append(a[i])
+for j in range(m):
+    existe=0
+    for i in range(n):
+        if(b[j==a[i]]):
+            existe=1
+    if existe==0:
+        c.append(b[j])
+print("Vector a:")
+print(a)
+print("Vector b:")
+print(b)
+print("Vector c:")
+print(c)
