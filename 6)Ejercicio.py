f4e4e351ffdbec48262b6eeb8170544c25aefe2c
jfNatanahel <natanahel.jf@gmail.com>
2023-10-16 21:24:01 -0300

    revisado

--- 6)Ejercicio.py
+++ 6)Ejercicio.py
@@ -3,10 +3,11 @@ lista1=[None]*m
 for i in range(m):
     lista1[i]=input("Ingresar caracteres L1: ")
 n=int(input("Ingresar el tamaño de la LISTA2: "))
-lista2=[None]*m
+lista2=[None]*n
 for j in range(n):
     lista2[j]=input("Ingersar caracteres L2: ")
-for i in range(n):
-    if lista1[i]==lista2[j]:
-        print(f"Carater {lista2} LISTA 2 se encuentra en la LISTA 1")
+for i in range(m):
+    for j in range(n):
+        if lista1[i]==lista2[j]:
+            print(f"Carater {lista2[j]} LISTA 2 se encuentra en la LISTA 1")
     
