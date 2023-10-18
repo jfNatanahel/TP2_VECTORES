8ec81d29dd4c0881dfc1206fe67045e43e6c45ad
jfNatanahel <natanahel.jf@gmail.com>
2023-10-18 09:20:59 -0300

    revisado

---
+++ 7)Ejercicio.py
@@ -0,0 +1,32 @@
+#Dado un vector de K elementos que contiene palabras, realizar un programa para eliminar las palabras
+#repetidas. El vector resultante debe ser un vector de m elemenntos no repetidos.
+n=int(input("Ingresar el tamaño del vector: "))
+v=[None]*n
+for i in range(n):
+    v[i]=input("Ingresar palabra")
+########ALGORITMO ELIMINAR ELEMENTOS REPETIDOS########
+"""
+¿CUAL ES LA IDEA DE LOS DOS CICLOS WHILE?
+La idea es que i representa el índice del elemento actual que estamos comparando, y j se utiliza
+para rastrear el índice del siguiente elemento en el vector que queremos comparar con el elemento
+en la posición i. Esto asegura que estemos comparando cada elemento con los elementos posteriores 
+en el vector para detectar duplicados.
+"""
+i=0
+while i<=n-2: #1er ciclo para "i"
+#¿Por qué i<=n-2?
+#primer elemento hasta el penúltimo elemento (hasta n-2).Esto se hace para seleccionar
+#un elemento en la posición i para compararlo con los elementos subsiguientes en el bucle interno.
+    j=i+1 #J toma el proximo valor de i entonces compara v[0]"i" con v[1]"j"
+    while j<=n-1:
+        if v[i]==v[j]:
+            for k in range(j,n-1):
+                v[k]=v[k+1]
+            n=n-1 #IMPORTANTE restarle uno al vector.
+        else:
+            j=j+1
+    i=i+1
+print("Lista sin repetidos:")
+for i in range(n):
+    print(v[i])
+print("Vector resultante es:",v[:n])
---
+++ 8)Ejercicio.py
@@ -0,0 +1,22 @@
+#Dada una lista de N números ordenados, insertar M números en los lugares que corresponda 
+#de tal manera que la lista preserve el orden.
+
+n=int(input("Ingresar el tamaño de la lista: "))
+v=[None]*(n+1)
+for i in range(n):
+    v[i]=int(input("Cargar los elementos del vector: "))
+m=int(input("Ingresar un numero para insertar en la lista en el lugar que corresponda: "))
+i=0
+b=True
+while (i<=n-1) and b:
+    if m>=v[i]: #El valor es mayor al V[i] AUN no encontramos la posicion para ubicarlo.
+        i=i+1 #Entonces aumentamos un lugar al "i" verficar con el siguiente numero.
+    else: #Encontramos el lugar para ubicar el numero.
+        n=n+1 #Aumentamos un lugar en la posicion de nuestro vector.
+        for k in range(n-1,i,-1): #Ciclo for para mover los lugares hacia la derecha. Por eso va desde n-1 hasta i.
+            v[k]=v[k-1]
+        v[i]=m #Se inserta el valor x en la posicion correcta i
+        b=False
+print("La lista con el valor insertado")
+for i in range(n):
+    print(v[i])
