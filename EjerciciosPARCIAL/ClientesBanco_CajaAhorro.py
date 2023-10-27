be957c412138bd950b2556afdd00cfe43086701a
jfNatanahel <natanahel.jf@gmail.com>
2023-10-27 19:21:36 -0300

    revisado

--- EjerciciosPARCIAL/ClientesBanco_CajaAhorro.py
+++ EjerciciosPARCIAL/ClientesBanco_CajaAhorro.py
@@ -1,3 +1,20 @@
+#Ejercicio
+"""
+Se debe realizar un programa en el cual inicialemnte se deben cargar dos vectores, uno con el DNI de
+los clientes del banco y el segundo con el saldo disponible de la caja de ahorro (ambos vectores en
+la misma posicion deben tener la informacion del mismo cliente).
+Luego se debe ingresar el DNI de un cliente y el saldo a extraer(repetir mientras el dni cargado sea
+distinto a cero).
+1. Calcular si el saldo del cliente es suficiente:
+a)Si el saldo es suficiente restar el monto a extraer, actualizando el valor del vector. Mostrar un 
+mensaje de "operacion exitosa" y mostrar el nuevo saldo.
+b)si el saldo no es suficiente mostrar el mensaje "Saldo insuficiente" y mostrar el saldo actual.
+2. Se debe llevar en una variable el monto total extraido satisfactoriamente, y en otra el monto que
+no se pudo extraer.
+3.Cuando se ingresa el DNI en cero(fin de la operatoria) se debe informar los montos totales extraidos
+y los montos totales que no se pudieron extraer, como asi tambien el saldo final de cada uno de los 
+clientes ordenados de menor a mayor de acuerdo al DNI
+"""
 #Ejemplo
 """
 Vector de Dni y en paralelo el otro Vector Saldos:
